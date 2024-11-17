b4edc1ae061a113ef39eb2a1a4146fd50806485f
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2024-11-17 21:12:20 +0545

    Prime or not

---
+++ Task3.dart
@@ -0,0 +1,21 @@
+bool isPrime(int num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (int i = 2; i <= num / 2; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void main() {
+  int number = 29;
+  
+  if (isPrime(number)) {
+    print('$number is a prime number.');
+  } else {
+    print('$number is not a prime number.');
+  }
+}
